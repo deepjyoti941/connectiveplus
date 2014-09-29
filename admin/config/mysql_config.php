1ccaa42d807a4bab49e15aacbcc1f8098df67855
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-09-29 16:31:54 +0530

    some config changes

--- admin/config/mysql_config.php
+++ admin/config/mysql_config.php
@@ -1,11 +1,11 @@
 <?php
 	//error_reporting(E_ALL);
 	error_reporting(0); 
-	$hostname = 'mysql.hostinger.in';
-	$username = 'u997856950_roy';
-	$password = 'lyZ6gE8tEM';
+	$hostname = '127.0.0.1';
+	$username = 'root';
+	$password = '';
 	try {
-	    $dbh = new PDO("mysql:host=localhost;dbname=u997856950_cvp", $username, $password);
+	    $dbh = new PDO("mysql:host=localhost;dbname=connectiveplus", $username, $password);
 	    }
 	catch(PDOException $e) {
 	    echo $e->getMessage();
