c4d406d7ad264436f43fd4d97d71d916cdc3a4bc
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-09-29 16:28:10 +0530

    some changes

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,10 +1,10 @@
 <?php 
 	//error_reporting(E_ALL);
 	error_reporting(0); 
-	$DB_HOST = '127.0.0.1';
-	$DB_USER = 'root';
-	$DB_PASS = '';
-	$DB_NAME = 'connectiveplus';
+	$DB_HOST = 'mysql.hostinger.in';
+	$DB_USER = 'u997856950_roy';
+	$DB_PASS = 'lyZ6gE8tEM';
+	$DB_NAME = 'u997856950_cvp';
 	$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);	
 
 ?>
--- admin/config/mysql_config.php
+++ admin/config/mysql_config.php
@@ -1,11 +1,11 @@
 <?php
 	//error_reporting(E_ALL);
 	error_reporting(0); 
-	$hostname = '127.0.0.1';
-	$username = 'root';
-	$password = '';
+	$hostname = 'mysql.hostinger.in';
+	$username = 'u997856950_roy';
+	$password = 'lyZ6gE8tEM';
 	try {
-	    $dbh = new PDO("mysql:host=localhost;dbname=connectiveplus", $username, $password);
+	    $dbh = new PDO("mysql:host=localhost;dbname=u997856950_cvp", $username, $password);
 	    }
 	catch(PDOException $e) {
 	    echo $e->getMessage();
